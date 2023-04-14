86927f24d18b304f0b8b02a5e9fbd957700dee58
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-04-14 20:06:12 +0900

    [week2]task1-데이터 배열 만들기

---
+++ week1,2/task1/constants/CUTE_ITEM_LIST.js
@@ -0,0 +1,62 @@
+export const CUTE_ITEM_LIST = [
+  {
+    id: 1,
+    name: "봉지",
+    hashtag: ["말티피그", "장봉지", "7짤"],
+    profile: "img/Bongji.jpg",
+  },
+  {
+    id: 2,
+    name: "밤비",
+    hashtag: ["꼬똥", "3짤", "고창석", "ㄱㅇㅇ💛"],
+    profile: "img/Bambi.jpg",
+  },
+  {
+    id: 3,
+    name: "후지",
+    hashtag: ["비숑", "강후지", "훚"],
+    profile: "img/Huji.jpg",
+  },
+  {
+    id: 4,
+    name: "고영희 짤",
+    hashtag: ["고얌이", "냥아치다", "ㄱㅇㅇ💛"],
+    profile: "img/Cat.JPG",
+  },
+  {
+    id: 5,
+    name: "행복한 쿼카",
+    hashtag: ["셀카장인", "스마일", "호주"],
+    profile: "img/Happy.jpg",
+  },
+  {
+    id: 6,
+    name: "춘식이",
+    hashtag: ["카카오프렌즈 춘식", "따땃"],
+    profile: "img/Chunsic.jpg",
+  },
+  {
+    id: 7,
+    name: "햄뿡이",
+    hashtag: ["햄뿡이", "방구뿡뿡", "내취향"],
+    profile: "img/Hambbung.jpg",
+  },
+  {
+    id: 8,
+    name: "최고심",
+    hashtag: ["할수이따", "긍정!!!", "체고"],
+    profile: "img/Gosim.JPG",
+  },
+  {
+    id: 9,
+    name: "명지",
+    hashtag: ["_myundi", "귀욤짱", "25짤"],
+    profile: "img/Myungji.jpg",
+  },
+  {
+    id: 10,
+    name: "햄찌짤",
+    hashtag: ["햄스터", "따봉짤", "최애짤"],
+    profile: "img/Hamjji.JPG",
+  },
+];
---
+++ week1,2/task1/script.js
@@ -0,0 +1,7 @@
+import { CUTE_ITEM_LIST } from "./constants/CUTE_ITEM_LIST";
+
+const startBtn = document.querySelector(".category-dog");
+
+startBtn.onclick = function () {
+  alert("자바스크립트 연결!📍");
+};
