9d6d11b2af4990c2365dfad31c0e432de031a5c3
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-04-17 22:41:28 +0900

    [week2]task1-태그 버튼 구현

--- week1,2/task1/script.js
+++ week1,2/task1/script.js
@@ -1,7 +1,74 @@
-import { CUTE_ITEM_LIST } from "./constants/CUTE_ITEM_LIST";
+// import { CUTE_ITEM_LIST } from "./constants/CUTE_ITEM_LIST";
 
-const startBtn = document.querySelector(".category-dog");
+const allBtn = document.querySelector(".category-all");
+const dogBtn = document.querySelector(".category-dog");
+const catBtn = document.querySelector(".category-cat");
+const ectBtn = document.querySelector(".category-ect");
+const textAllBtn = document.querySelector(".text--all");
+const textDogBtn = document.querySelector(".text--dog");
+const textCatBtn = document.querySelector(".text--cat");
+const textEctBtn = document.querySelector(".text--ect");
 
-startBtn.onclick = function () {
-  alert("자바스크립트 연결!📍");
+allBtn.onclick = function () {
+  const selectAll = document.getElementById("category-all").value;
+  const textAll = document.querySelector("#text--all");
+  textAll.style.display = "block";
+  document.getElementById("text--all").innerText = selectAll;
+};
+
+textAllBtn.onclick = function () {
+  const textAllClick = document.querySelector("#text--all");
+  if (textAllClick.style.display === "none") {
+    textAllClick.style.display = "block";
+  } else {
+    textAllClick.style.display = "none";
+  }
+};
+
+dogBtn.onclick = function () {
+  const selectDog = document.getElementById("category-dog").value;
+  const textDog = document.querySelector("#text--dog");
+  textDog.style.display = "block";
+  document.getElementById("text--dog").innerText = selectDog;
+};
+
+textDogBtn.onclick = function () {
+  const textDogClick = document.querySelector("#text--dog");
+  if (textDogClick.style.display === "none") {
+    textDogClick.style.display = "block";
+  } else {
+    textDogClick.style.display = "none";
+  }
+};
+
+catBtn.onclick = function () {
+  const selectCat = document.getElementById("category-cat").value;
+  const textCat = document.querySelector("#text--cat");
+  textCat.style.display = "block";
+  document.getElementById("text--cat").innerText = selectCat;
+};
+
+textCatBtn.onclick = function () {
+  const textCatClick = document.querySelector("#text--cat");
+  if (textCatClick.style.display === "none") {
+    textCatClick.style.display = "block";
+  } else {
+    textCatClick.style.display = "none";
+  }
+};
+
+ectBtn.onclick = function () {
+  const selectEct = document.getElementById("category-ect").value;
+  const textEct = document.querySelector("#text--ect");
+  textEct.style.display = "block";
+  document.getElementById("text--ect").innerText = selectEct;
+};
+
+textEctBtn.onclick = function () {
+  const textEctClick = document.querySelector("#text--ect");
+  if (textEctClick.style.display === "none") {
+    textEctClick.style.display = "block";
+  } else {
+    textEctClick.style.display = "none";
+  }
 };
