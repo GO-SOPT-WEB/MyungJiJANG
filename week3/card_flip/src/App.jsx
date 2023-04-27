beee1f1bce7c8c09758f145a91ad408e1afdd434
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-04-27 17:02:44 +0900

    [week3]폴더 구조 및 데이터 설정

---
+++ week3/card_flip/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./component/Header";
+
+function App() {
+  //필요한 기능 1. 게임 난이도 선택
+  // 2. 정답 노출
+  // 3. 카드 선택
+  // 4. 카드 배열 랜덤
+  // 상수파일 -> 데이터 랜더링
+
+  //컴포 분리 => 헤더->한 번더 분리? (제목/리셋 버튼/ 스코어) =>  cardBody (난이도 선택/ card리스트)
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ week3/card_flip/src/constants/CARD_LIST.js
@@ -0,0 +1,48 @@
+import MyCarF from "../assets/Ferrari.PNG";
+import MyCarL from "../assets/Lamborghini.PNG";
+import MyMacP from "../assets/MacPro.PNG";
+import MyImac from "../assets/Imac.JPG";
+import MyHouse from "../assets/House.JPG";
+import MyBag from "../assets/LouisVuitton.JPG";
+import MyPlane from "../assets/PrivatePlane.JPG";
+import MyCarV from "../assets/Volvo.JPG";
+import MyGold from "../assets/GoldBar.JPG";
+
+export const CARD_LIST = [
+  {
+    id: 1,
+    image: MyCarF,
+  },
+  {
+    id: 2,
+    image: MyCarL,
+  },
+  {
+    id: 3,
+    image: MyMacP,
+  },
+  {
+    id: 4,
+    image: MyImac,
+  },
+  {
+    id: 5,
+    image: MyHouse,
+  },
+  {
+    id: 6,
+    image: MyBag,
+  },
+  {
+    id: 7,
+    image: MyGold,
+  },
+  {
+    id: 8,
+    image: MyCarV,
+  },
+  {
+    id: 9,
+    image: MyPlane,
+  },
+];
