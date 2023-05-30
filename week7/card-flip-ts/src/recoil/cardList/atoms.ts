f67609b554417945fa09dfb53c18b43b9eb566f2
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-05-30 11:22:53 +0900

    [feat]리코일 상태 선언

--- week7/card-flip-ts/src/recoil/cardList/atoms.ts
+++ week7/card-flip-ts/src/recoil/cardList/atoms.ts
@@ -1,6 +1,36 @@
 import { atom } from "recoil";
 
-export const myungCardList = atom({
-  key: "myungCardList",
-  default: "",
+export const cardState = atom({
+  key: "cardState",
+  default: [],
+});
+
+export const firstChoiceState = atom({
+  key: "firstChoiceState",
+  default: null,
+});
+
+export const secondCoiceState = atom({
+  key: "secondChoiceState",
+  default: null,
+});
+
+export const disabledState = atom({
+  key: "disabledState",
+  default: false,
+});
+
+export const scoreState = atom({
+  key: "scoreState",
+  default: 0,
+});
+
+export const isGameOverState = atom({
+  key: "isGameOverState",
+  default: false,
+});
+
+export const animationTriggerState = atom({
+  key: "animationTriggerState",
+  default: false,
 });
